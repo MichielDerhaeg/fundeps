b118d6523da34811158a46d608d6258c00955f75
Michiel Derhaeg <derhaeg.michiel@gmail.com>
2018-03-21 23:36:56 +0100

    most of wanted interact

--- prototype/Frontend/HsEntail.hs
+++ prototype/Frontend/HsEntail.hs
@@ -17,6 +17,8 @@ data WantedCt
   = WantedEqCt WantedEqCt
   | WantedClsCt WantedClsCt
 
+type WantedCs = [WantedCt]
+
 type GivenEqCt = Ann FcCoercion EqCt
 
 type GivenClsCt = Ann FcTerm RnClsCt
@@ -26,6 +28,8 @@ data GivenCt
   | GivenClsCt GivenClsCt
 
 -- | Substitute an equality within a type and generate a coercion.
+-- This is weird, type type signature could be more precise.
+-- Instead of returning what we passed and throwing it away.
 coTy :: GivenEqCt -> RnMonoTy -> GivenEqCt
 coTy ct@(_co :| (TyVar a :~: _ty)) (TyVar b)
   | a == b = ct
@@ -77,3 +81,55 @@ isOrphan TyCon {} = True
 isOrphan (TyApp ty1 ty2) = isOrphan ty1 && isOrphan ty2
 isOrphan TyVar {} = True
 isOrphan TyFam {} = False
+
+interactWanted :: WantedCt -> WantedCt -> TcM (WantedCs, EvSubst)
+interactWanted (WantedEqCt (c1 :| ct1@(TyVar a :~: ty1)))
+               (WantedEqCt (c2 :| ct2@(TyVar b :~: ty2)))
+  -- EQSAME
+  | a == b, isCan (EqualityCt ct1), isCan (EqualityCt ct2) = do
+    c2' <- freshFcCoVar
+    return
+      ( WantedEqCt <$> [c1 :| ct1, c2' :| (ty1 :~: ty2)]
+      , coToEvSubst (c2 |-> FcCoTrans (FcCoVar c1) (FcCoVar c2')))
+  -- EQDIFF
+  | a `elem` ftyvsOf ty2, isCan (EqualityCt ct1), isCan (EqualityCt ct2)
+  , let (co :| (_ty1 :~: sub_ty2)) = coTy (FcCoVar c1 :| ct1) ty2 = do
+    c2' <- freshFcCoVar
+    return
+      ( WantedEqCt <$> [c1 :| ct1, c2' :| (TyVar b :~: sub_ty2)]
+      , coToEvSubst (c2 |-> FcCoTrans (FcCoVar c2') (FcCoSym co)))
+interactWanted (WantedEqCt (c1 :| ct1@(TyVar     a :~: _ty1)))
+               (WantedEqCt (c2 :|     (TyFam f tys :~:  ty2)))
+  -- EQFEQ
+  | isCan (EqualityCt ct1), a `elem` ftyvsOf (ty2 : tys)
+  , let (co1 :| (_ :~: sub_tyfam)) = coTy (FcCoVar c1 :| ct1) (TyFam f tys)
+  , let (co2 :| (_ :~: sub_ty2  )) = coTy (FcCoVar c1 :| ct1) ty2 = do
+    c2' <- freshFcCoVar
+    return
+      ( WantedEqCt <$> [ c1 :| ct1, c2' :| (sub_tyfam :~: sub_ty2)]
+      , coToEvSubst (c2 |-> FcCoTrans co1 (FcCoTrans (FcCoVar c2') (FcCoSym co2))))
+interactWanted (WantedEqCt  (c :| ct1@(TyVar a :~: _ty)))
+               (WantedClsCt (d :| ct2@(ClsCt cls   tys)))
+  -- EQDICT
+  | isCan (EqualityCt ct1), a `elem` ftyvsOf tys
+  , let (co :| (_ :~: sub_cls)) = coCt (FcCoVar c :| ct1) ct2
+  , let sub_tys = undefined sub_cls = do -- TODO
+    d' <- freshDictVar
+    return
+      ( [WantedEqCt (c :| ct1), WantedClsCt (d' :| (ClsCt cls sub_tys))]
+      , tmToEvSubst (d |-> FcTmCast (FcTmVar d') (FcCoSym co)))
+interactWanted (WantedEqCt (c1 :| ct1@(TyFam _f1 _tys1 :~: ty1)))
+               (WantedEqCt (c2 :|     (TyFam _f2 _tys2 :~: ty2))) = do
+  -- FEQFEQ
+    c2' <- freshFcCoVar
+    return
+      ( WantedEqCt <$> [c1 :| ct1, c2' :| (ty1 :~: ty2)]
+      , coToEvSubst (c2 |-> FcCoTrans (FcCoVar c1) (FcCoVar c2')))
+interactWanted (WantedClsCt (d1 :| ClsCt cls1 tys1))
+               (WantedClsCt (d2 :| ClsCt cls2 tys2))
+  -- DDICT
+  | tys1 `undefined` tys2, cls1 == cls2 = do -- TODO type equality
+    return
+      ( [WantedClsCt (d1 :| ClsCt cls1 tys1)]
+      , tmToEvSubst (d2 |-> FcTmVar d1))
+interactWanted _ _ = error "TODO"
