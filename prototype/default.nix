3b00d344185962c42578fb918dda8e66f777bb3c
Michiel Derhaeg <derhaeg.michiel@gmail.com>
2018-03-29 00:48:28 +0200

    update nix expr with megaparsec

--- prototype/default.nix
+++ prototype/default.nix
@@ -1,10 +1,10 @@
-{ mkDerivation, base, mtl, parsec, pretty, stdenv }:
+{ mkDerivation, base, mtl, megaparsec, pretty, stdenv }:
 mkDerivation {
   pname = "prototype";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base mtl parsec pretty ];
+  executableHaskellDepends = [ base mtl megaparsec pretty ];
   license = stdenv.lib.licenses.bsd3;
 }
