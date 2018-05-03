bc691cb6962d146c3cd3e50fe9d8c4503fa899fb
Michiel Derhaeg <derhaeg.michiel@gmail.com>
2018-05-04 00:56:26 +0200

    add callstack with CPP pragma's

--- prototype/Utils/Utils.hs
+++ prototype/Utils/Utils.hs
@@ -1,15 +1,28 @@
+{-# LANGUAGE CPP #-}
 
 module Utils.Utils where
 
-import Data.List (nub)
-import Control.Monad (unless, when)
+import           Control.Monad (unless, when)
+import           Data.List     (nub)
+
+#if __GLASGOW_HASKELL__ >= 800
+import GHC.Stack (HasCallStack)
+#endif
 
 -- | Zip two lists into a list of tuples. Fail if lengths don't match.
+#if __GLASGOW_HASKELL__ < 800
 zipExact :: [a] -> [b] -> [(a,b)]
+#else
+zipExact :: HasCallStack => [a] -> [b] -> [(a,b)]
+#endif
 zipExact = zipWithExact (,)
 
 -- | Zip two lists into a list using a combining function. Fail if lengths don't match.
+#if __GLASGOW_HASKELL__ < 800
 zipWithExact :: (a -> b -> c) -> [a] -> [b] -> [c]
+#else
+zipWithExact :: HasCallStack => (a -> b -> c) -> [a] -> [b] -> [c]
+#endif
 zipWithExact _f []     []     = []
 zipWithExact  f (x:xs) (y:ys) = f x y : zipWithExact f xs ys
 zipWithExact _f _      _      = error "zipWithExact: length mismatch"
