f12e110bdb68438bc313ad9323042b79c895bc43
George Karachalias <george.karachalias@gmail.com>
2018-05-07 17:22:43 +0200

    Added a couple of determinacy tests

---
+++ prototype/Tests/determinacy/FDs10.hs
@@ -0,0 +1,28 @@
+
+data List (a :: *) = Nil | Cons a (List a)
+
+class forall (a :: *) (b :: *). G a b | a -> b where
+  gop :: a -> a
+
+class forall (a :: *) (b :: *). H a b | a -> b where
+  hop :: a -> a
+
+class forall (a :: *) (b :: *). F a b | a -> b where
+  fop :: a -> a
+
+-- undefined :: forall a. a
+undefined = let x = x in x
+
+instance forall (a :: *) (b :: *) (c :: *). (G a c, H c b) => F (List a) (List b) where
+  fop = undefined
+
+-- SHOULD SUCCEED
+
+-- CURRENTLY FAILS WITH THIS:
+--
+-- || Ambigiuous context : bs : [c_uI],
+-- ||                      as : [a_uJ, b_uK, c_uI],
+-- ||                      class constraints : [G a_uJ c_uI, H c_uI b_uK]
+
+\x. x
+
---
+++ prototype/Tests/determinacy/FDs11.hs
@@ -0,0 +1,30 @@
+
+data Nat = Zero | Succ Nat
+data List (a :: *) = Nil | Cons a (List a)
+data Tup (a :: *) (b :: *) = MkTup a b
+
+class forall (a :: *) (b :: *). G a b | a -> b where
+  gop :: a -> a
+
+class forall (a :: *) (b :: *). H a b | a -> b where
+  hop :: a -> a
+
+class forall (a :: *) (b :: *). F a b | a -> b where
+  fop :: a -> a
+
+-- undefined :: forall a. a
+undefined = let x = x in x
+
+instance forall (a :: *) (b :: *) (c :: *). (G a (Tup a Nat), H c b) => F (List a) (List b) where
+  fop = undefined
+
+-- SHOULD SUCCEED
+
+-- CURRENTLY FAILS WITH THIS:
+--
+-- || Ambigiuous context : bs : [c_uR],
+-- ||                      as : [a_uP, b_uQ, c_uR],
+-- ||                      class constraints : [G a_uP (Tup a_uP Nat), H c_uR b_uQ]
+
+\x. x
+
