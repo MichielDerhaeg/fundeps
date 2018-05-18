fff4c9fc71926cbb0857e42e31463b76eae66f22
Michiel Derhaeg <derhaeg.michiel@gmail.com>
2018-05-18 20:19:11 +0200

    autogenerated shell file

--- prototype/shell.nix
+++ prototype/shell.nix
@@ -1,4 +1,32 @@
-with (import <nixpkgs> {}).pkgs;
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default", doBenchmark ? false }:
+
 let
-  prototype = pkgs.haskellPackages.callPackage ./. {};
-in prototype.env
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, base, megaparsec, mtl, pretty, stdenv
+      , transformers
+      }:
+      mkDerivation {
+        pname = "prototype";
+        version = "0.1.0.0";
+        src = ./.;
+        isLibrary = false;
+        isExecutable = true;
+        executableHaskellDepends = [
+          base megaparsec mtl pretty transformers
+        ];
+        license = stdenv.lib.licenses.bsd3;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
+
+  drv = variant (haskellPackages.callPackage f {});
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
